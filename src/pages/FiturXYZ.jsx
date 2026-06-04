4f1be9bcf07dd2b98b6afc8c816be13d918dc2e9
wirdaini <wirda24si@mahasiswa.pcr.ac.id>
2026-06-04 15:37:44 +0700

    feat: tambah komponen navbar

--- src/pages/FiturXYZ.jsx
+++ src/pages/FiturXYZ.jsx
@@ -1,23 +1,48 @@
-import { useState } from "react";
+
 import PageHeader from "../components/PageHeader";
-
+import {Button} from "@/components/ui/Button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
 export default function FiturXYZ() {
-  const [showForm, setShowForm] = useState(false);
+    return (
+        <div id="dashboard-container">
+            <PageHeader title="Fitur XYZ" />
+            <p>Ini adalah halaman untuk fitur XYZ yang sedang dalam pengembangan. Nantikan update selanjutnya!</p>
 
-  return (
-    <div className="space-y-6">
-      <PageHeader
-        title="Fitur XYZ"
-        breadcrumb={["Dashboard", "Fitur XYZ"]}
-      >
-        <button
-          onClick={() => setShowForm(!showForm)}
-          className="bg-hijau text-white px-4 py-2 rounded-xl shadow hover:scale-105 transition"
-        >
-          + Add Feature
-        </button>
-      </PageHeader>
-    </div>
-  );
+            <Button variant="outline">Simpan</Button>
+            <Button variant="default">Simpan</Button>
+            <Button variant="secondary">Simpan</Button>
+            <Button variant="destructive">Simpan</Button>
+
+            <Card>
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                    <CardAction>Card Action</CardAction>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+
+            <Badge>Default</Badge>
+      <Badge variant="secondary">Secondary</Badge>
+      <Badge variant="destructive">Destructive</Badge>
+      <Badge variant="outline">Outline</Badge>
+      <Badge variant="ghost">Ghost</Badge>
+        </div>
+
+
+    );
 }
-
